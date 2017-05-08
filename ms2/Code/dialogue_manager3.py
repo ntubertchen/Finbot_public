3be4c3c0d1888d18350528a7a38e4dadbac88461
nturivengod <b02902046@ntu.edu.tw>
2017-05-09 04:17:31 +0800

    Update dialogue_manager3.py

--- ms2/Code/dialogue_manager3.py
+++ ms2/Code/dialogue_manager3.py
@@ -1,5 +1,6 @@
 #from state import State
 #from nn_model import NN
+from episoda import episode
 from rule_simulator import RuleSimulator, initializer
 #import tensorflow as tf
 #from tensorflow.contrib import rnn
@@ -19,7 +20,7 @@ def run_an_episode():
 	user = RuleSimulator()
 	print ("New episode, user goal:")
 	user.goal.dump()
-	print('_ _ _ _ _ _ _ _ _ _ _ _')
+	print('==========================')
 	over = user.episode_over
 	nl_input = initializer(user.goal)
 	turn_by_turn(currturn,nl_input,'user')
@@ -27,12 +28,47 @@ def run_an_episode():
 	while (not over):
 		#pdb.set_trace()
 		frame_output = {}
-		prompt = "enter a system_action in string type (confirm,request,response):"
+		prompt = "\nenter system_action:[confirm_answer,request,response,closing]\n->"
 		a = input(prompt)	
-		while a not in ["confirm","request","response"]:
+		while a not in ["confirm_answer","request","response",'closing']:
 			a = input(prompt)
-		print ("got",a)
-		pdb.set_trace()
+		frame_output['system_action'] = [a]
+		frame_output['action_item'] = [user.goal.goal]	
+		if frame_output['system_action'][0] == 'confirm_answer':
+			prompt = "#enter a slot :["
+			for i in user.goal.inform_slots:
+				prompt += str(i)
+				prompt += ","
+			prompt += "]\n->"
+			a = input(prompt)
+			while a not in user.goal.inform_slots:
+				a = input(prompt)
+			frame_output['slot'] = {}
+			prompt = "#enter value for slot {}\n->".format(a)
+			b = input(prompt)
+			frame_output['slot'][a] = b
+		elif frame_output['system_action'][0] == 'response':
+			frame_output['slot'] = {}
+			for i in user.goal.inform_slots:
+				prompt = "#enter value for {}\n->".format(i)
+				v = input(prompt)
+				frame_output['slot'][i] = v
+		elif frame_output['system_action'][0] == 'request':
+			prompt = "#choose a slot to request:["
+			s = user.goal.inform_slots
+			if 'symbol' in s:
+				del s['symbol'] 
+			for i in s:
+				prompt += str(i)
+				prompt += ','
+			prompt += "]\n->"
+			a = input(prompt)
+			while a not in s:
+				a = input(prompt)
+			frame_output["slot"] = [a]
+		elif frame_output['system_action'][0] == 'closing':
+			frame_output['action_item'] = ['closing']
+			frame_output['slot'] = []
 		#system side
 		#slot,intent = nlu.predict(nl_input,system)
 		#system.update(slot,intent,nl_input)
@@ -64,10 +100,8 @@ def run_an_episode():
 		over = user.episode_over
 		
 	if user.success:
-		print("Successful")
 		reward += 2*maxturn
 	else:
-		print("Failing")
 		reward -= maxturn
 
 	return reward,user.success,currturn
@@ -77,6 +111,7 @@ if __name__ == '__main__':
 	count = 1.
 	succ = 0.
 	turn = 0.
+	reward = 0.
 	#config = tf.ConfigProto()
 	#config.gpu_options.allow_growth = True
 	#config.gpu_options.per_process_gpu_memory_fraction = 0.2
@@ -85,7 +120,11 @@ if __name__ == '__main__':
 	#nlu.loadmodel()
 	for _ in range(30):
 		r,s,c = run_an_episode()
-		if s is True: succ+=1
+		if s is True: 
+			succ+=1
 		turn += c
-		print ('reward:',r,';\trate:',succ/count,"avg_turn:",c/count)
+		reward += r
+		print("--- end of episode ---")
+		print('reward: {0} ,it was a {1} dialogue.'.format(r,'successful' if s is True else 'failing'))
+		print('avg_reward: {0}\nsucc_rate: {1}\navg_turns: {2}\n\n========================='.format(reward/count, succ/count, turn/count))
 		count+=1
